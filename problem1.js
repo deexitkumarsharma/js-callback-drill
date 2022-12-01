420db35bb42edaa9b972ebfaff8f909cabb220e2
Deexit Kumar Sharma <deexitkumarsharma@gmail.com>
2022-12-01 19:25:07 +0530

    update

--- problem1.js
+++ problem1.js
@@ -8,31 +8,29 @@
 
 const fs = require("fs");
 
-let myFunc = (() => {
-    fs.mkdir("../json", () => {
-        for (let i = 0; i < 8; i++) {
-
-            let path ="../json" + Math.random().toString(36).slice(-5); + ".json";
-            let data = {};
-
-            fs.writeFile(path,JSON.stringify(data,null,2), ((error) => {
-                if (error) {
-                    console.log("error occurred while creating file!", err);
-                    return;
-                } else {
-                    console.log("File created successfully  " + path);
-                }
-                fs.unlink(path, ((error) => {
-                    if (error) {
-                        throw error;
-                    } else {
-                        console.log("File deleted successfully" + path);
-                    }
-                }));
-            }));
+let myFunc = () => {
+  fs.mkdir("../json", () => {
+    for (let index = 0; index < 8; index++) {
+      let path = "../json" + Math.random().toString(36).slice(-5);
+      +".json";
+      let data = {};
+      fs.writeFile(path, JSON.stringify(data, null, 2), (error) => {
+        if (error) {
+          console.log("error occurred while creating file!", err);
+          return;
+        } else {
+          console.log("File created successfully  " + path);
         }
-    });
-});
-
+        fs.unlink(path, (error) => {
+          if (error) {
+            throw error;
+          } else {
+            console.log("File deleted successfully" + path);
+          }
+        });
+      });
+    }
+  });
+};
 
 module.exports = myFunc;
--- problem2.js
+++ problem2.js
@@ -11,107 +11,101 @@
 
 const fs = require("fs");
 
-let myFunc2 = () =>{
-    fs.readFile("lipsum.txt", "utf-8", (error, data) => {
-        console.log(data)
-        let path = "upperCase.txt";
-        let upperCaseData = data.toUpperCase();
-//2nd Step convert the content to uppercase & write to a new file.
+let myFunc2 = () => {
+  fs.readFile("lipsum.txt", "utf-8", (error, data) => {
+    console.log(data);
+    let path = "upperCase.txt";
+    let upperCaseData = data.toUpperCase();
+    //2nd Step convert the content to uppercase & write to a new file.
 
-        fs.writeFile(path, upperCaseData, (err)=>{
-            if(err){
-                throw err;
+    fs.writeFile(path, upperCaseData, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(upperCaseData);
+
+      fs.rename("upperCase.txt", "filenames.txt", () => {
+        console.log("\nupperCase File Renamed!\n");
+
+        fs.readFile("filenames.txt", "utf-8", (err, upperCaseReadData) => {
+          console.log(upperCaseReadData);
+
+          // 3.convert it to lower case. Then split the contents into sentences. Then write it to a new file. Store the name of the new file in filenames.txt
+          let newpath = "filenames.txt";
+          let lowerCaseData = upperCaseData.toLowerCase();
+          fs.writeFile(newpath, lowerCaseData, (err) => {
+            if (err) {
+              throw err;
             }
-            console.log(upperCaseData);
+            console.log(lowerCaseData);
 
-            fs.rename('upperCase.txt', 'filenames.txt', () => {
-                console.log("\nupperCase File Renamed!\n");
+            var dataArr = data.split(". ");
+            // console.log(dataArr);
+            let splitContent = dataArr.reduce((acc, curr) => {
+              let newArr = acc + "\n" + curr;
+              return newArr;
+            }, "");
+            // console.log(res)
 
-                fs.readFile("filenames.txt", "utf-8", (err, upperCaseReadData) =>{
-                    console.log(upperCaseReadData);
+            let newpath2 = "filenames.txt";
 
-// 3.convert it to lower case. Then split the contents into sentences. Then write it to a new file. Store the name of the new file in filenames.txt                  
-                    let newpath = "filenames.txt";
-                    let lowerCaseData = upperCaseData.toLowerCase();
-                    fs.writeFile(newpath, lowerCaseData, (err)=>{
-                        if(err){
-                            throw err;
-                        }
-                        console.log(lowerCaseData);
+            fs.writeFile(newpath2, splitContent, (err) => {
+              if (err) {
+                throw err;
+              }
+              // console.log(splitContent)
 
-                        var dataArr = data.split('. ');  
-                        // console.log(dataArr);
-                        let splitContent = dataArr.reduce((acc, curr)=>{
-                            let n = acc + "\n" + curr;
-                            return n;
-                        },'')
-                        // console.log(res) 
+              fs.readFile("filenames.txt", "utf-8", (err, afterstep3data) => {
+                // console.log(afterstep3data);
+                //4. Read the new files, sort the content, write it out to a new file. Store the name of the new file in filenames.txt
 
-                        let newpath2 = "filenames.txt"; 
+                var sortContent = afterstep3data.split("\n").sort().join("\n");
+                let path = "sorted.txt";
+                fs.writeFile(path, sortContent, (err) => {
+                  if (err) {
+                    throw err;
+                  }
+                  // console.log(sortContent);
 
-                        fs.writeFile(newpath2, splitContent, (err)=>{
-                            if(err){
-                                throw err;
+                  let newpath = "filenames.txt";
+                  fs.writeFile(newpath, sortContent, (err) => {
+                    if (err) {
+                      throw err;
+                    }
+
+                    fs.appendFile("filenames.txt", sortContent, (err) => {
+                      if (err) {
+                        throw err;
+                      }
+                      // 5. Read the contents of filenames.txt and delete all the new files that are mentioned in that list simultaneously.
+                      fs.readFile(
+                        "filenames.txt",
+                        "utf-8",
+                        (err, finalData) => {
+                          console.log(finalData);
+
+                          var files = ["filenames.txt", "sorted.txt"];
+                          // myListOfFiles will be your list of files with paths relative to your script.
+                          function deleteFiles(files) {
+                            for (const file of files) {
+                              fs.unlink(file, (err) => {
+                                if (err) throw err;
+                              });
                             }
-                            // console.log(splitContent)
-
-                            fs.readFile("filenames.txt", "utf-8", (err, afterstep3data)=>{
-                                // console.log(afterstep3data);
-//4. Read the new files, sort the content, write it out to a new file. Store the name of the new file in filenames.txt
-
-                                var sortContent = afterstep3data.split("\n").sort().join("\n");
-                                let path = "sorted.txt"
-                                fs.writeFile(path, sortContent, (err) =>{
-                                    if(err){
-                                        throw err;
-                                    }
-                                    // console.log(sortContent);
-
-                                    let newpath = "filenames.txt";
-                                    fs.writeFile(newpath, sortContent, (err)=>{
-                                        if(err){
-                                            throw err;
-                                        }
-
-                                        fs.appendFile("filenames.txt", sortContent, (err)=>{
-                                            if(err){
-                                                throw err;
-                                            }
-// 5. Read the contents of filenames.txt and delete all the new files that are mentioned in that list simultaneously.
-                                            fs.readFile("filenames.txt", "utf-8", (err, finalData)=>{
-                                                console.log(finalData)
-
-                                                    var files = ['filenames.txt', 'sorted.txt'];
-                                                    // myListOfFiles will be your list of files with paths relative to your script.
-                                                    function deleteFiles(files){
-                                                        for (const file of files){
-                                                            fs.unlink((file), err =>{
-                                                                if(err)
-                                                                throw err;
-                                                            })
-                                                        }
-                                                    }
-                                                    deleteFiles(files);
-                                                
-                                            })
-
-
-                                        })
-                                    
-                                    })
-
-                                })
-                            })
-                        })
-
-
-                    })
-
-                })
-            
-        })
-    })
-})
-}
+                          }
+                          deleteFiles(files);
+                        }
+                      );
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+};
 
 module.exports = myFunc2;
